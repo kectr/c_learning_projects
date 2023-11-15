6c356e4cda7a19f667abba86c26b68d551db536c
kectr <148006014+kectr@users.noreply.github.com>
2023-11-15 13:33:01 +0300

    Doen homework

---
+++ 3rd_week/deneme.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct insan{
+	int yas;
+	int boy;
+}
+
+int main(){
+	struct insan hasan;
+	hasan.yas = 10;
+	hasan.boy = 150;
+	
+	printf("benim adim hasam boyum %d",hasan.boy);
+}
